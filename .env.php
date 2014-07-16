d8103b67d3a66fb22ccfef7849f85ccc6c6da48a
yboker <8236138@qq.com>
2014-07-16 13:49:04 +0800

    aa

---
+++ .env.php
@@ -0,0 +1,13 @@
+<?php
+return array
+(
+    'APP_DEBUG'          => true,
+    'DATABASE_DRIVER'    => 'mysql',
+    'DATABASE_HOST'      => 'localhost',
+    'DATABASE_DATABASE'  => 'my_laravel',
+    'DATABASE_USERNAME'  => 'root',
+    'DATABASE_PASSWORD'  => '',
+    'DATABASE_CHARSET'   => 'utf-8',
+    'DATABASE_COLLATION' => 'utf8_unicode_ci',
+    'DATABASE_PREFIX'    => 'laravel_'
+);
